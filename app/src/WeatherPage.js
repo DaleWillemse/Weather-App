df6e3d8bc3ce1526fdc92c0f293701099ea9abb8
Dale Willemse <DaleWillemse000@gmail.com>
2023-06-02 19:57:45 +0200

    starting css for WeatherPage

---
+++ app/src/WeatherPage.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./WeatherPage.css";
+
+function WeatherPage() {
+  return (
+    <div>
+      <div className="AreaData">
+        <h1 id="AreaName">Area Name</h1>
+        <h2 id="Date">31 May 2023</h2>
+      </div>
+    </div>
+  );
+}
+
+export default WeatherPage;
