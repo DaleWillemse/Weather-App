ac4685d5c8a02b04599ba0079a5f9aa0eb19b24e
Fenrir-23 <germangeneral2301@gmail.com>
2023-06-03 20:35:05 +0200

    Fixed Broken Lin's error from Cherrypick commit

--- app/src/App.js
+++ app/src/App.js
@@ -1,8 +1,4 @@
 import "./App.css";
-import CurrentWeather from "./current-weather/current-weather";
-import Forecast from "./forecast/forecast";
-import SearchCity from "./components/searchCity";
-import { WEATHER_API_URL, WEATHER_API_KEY } from "./api";
 import { Routes, Route } from "react-router-dom";
 import WeatherPage from "./WeatherPage";
 import HomePage from "./HomePage";
