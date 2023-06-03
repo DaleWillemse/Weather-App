3f721c9252cad4522b2a1a0e83ff005c71fdeb3e
Dale Willemse <DaleWillemse000@gmail.com>
2023-06-03 21:52:26 +0200

    merging overhaul and my branch

--- app/src/App.js
+++ app/src/App.js
@@ -4,39 +4,7 @@ import WeatherPage from "./WeatherPage";
 import HomePage from "./HomePage";
 
 function App() {
-  const [currentWeather, setCurrentWeather] = useState(null);
-  const [forecast, setForecast] = useState(null);
-
-  const handleOnSearchChange = (searchData) => {
-    const [lat, lon] = searchData.value.split(" ");
-
-    const currentWeatherFetch = fetch(
-      `${WEATHER_API_URL}/weather?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}&units=metric`
-    );
-    const forecastFetch = fetch(
-      `${WEATHER_API_URL}/forecast?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}&units=metric`
-    );
-
-    Promise.all([currentWeatherFetch, forecastFetch])
-      .then(async (response) => {
-        const weatherResponse = await response[0].json();
-        const forcastResponse = await response[1].json();
-
-        setCurrentWeather({ city: searchData.label, ...weatherResponse });
-        setForecast({ city: searchData.label, ...forcastResponse });
-      })
-      .catch(console.log);
-  };
-
   return (
-    <body>
-      <div className="App">
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/WeatherPage" element={<WeatherPage />} />
-        </Routes>
-      </div>
-    </body>
     <body>
       <div className="App">
         <Routes>
