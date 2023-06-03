7d362c2610b37c8ef322f043097e9aacdf9a8bf4
Fenrir-23 <germangeneral2301@gmail.com>
2023-06-03 21:27:42 +0200

    Dependencies and config.

---
+++ server/App.js
@@ -0,0 +1,19 @@
+//Config File
+require('dotenv').config();
+
+//Port can't be the same as client/app
+const PORT = process.env.PORT;
+const KEY = process.env.KEY;
+
+//Build express server
+const express = require('express');
+const app = express();
+
+// Data Array
+const data = [];
+
+
+//runs server
+app.listen(PORT, () => {
+    console.log(`Server is running on port: ${port}`);
+})
