0c560f04e2af67589115d300bb62e395c4b49e8c
Fenrir-23 <germangeneral2301@gmail.com>
2023-06-03 14:25:44 +0200

    Dynamic Weather Icons

--- app/src/components/weather_icon.js
+++ app/src/components/weather_icon.js
@@ -1,38 +1,38 @@
 //https://openweathermap.org/weather-conditions ---for weather types
 // App.js requires --- import WeatherIcon from './components/weather_icon';
+// Condition passed --- <WeatherIcon condition='Cloudy'/>  
 import React from 'react';
 import './weather_icon.css';
 
 const weatherIcon = props => {
-    let icon = '';
+    let icon = "";
     switch(props.condition){
         case 'Thunderstorm':
-            icon ='./public/weather_icons/thunderstorm.png';
+            icon ='/weather_icons/thunderstorm.png';
             break;
         case 'Drizzle':
-            icon ='./public/weather_icons/overcast.png';
+            icon ='/weather_icons/overcast.png';
             break;
         case 'Rain':
-            icon ='./public/weather_icons/rainy.png';
+            icon ='/weather_icons/rainy.png';
             break;
         case 'Snow':
-            icon ='./public/weather_icons/snowy.png';
+            icon ='/weather_icons/snowy.png';
             break;
         case 'Clear':
-            icon ='./public/weather_icons/sunshine.png';
+            icon ='/weather_icons/sunshine.png';
             break;
         case 'Clouds':
-            icon ='./public/weather_icons/cloudy.png';
+            icon ='/weather_icons/cloudy.png';
             break;
         default: // Atmosphere has multiple dofferent conditions
-            icon ='./public/weather_icons/foggy.png';
+            icon ='/weather_icons/foggy.png';
             break;                    
     }
-    //Have extra windy Icon
 
     //Switch works Icon is jus not assigned
     return (
-    <icon className="icon" src={icon} alt="Missing Weather Icon"/>
+    <img className="icon" src={icon} alt="Missing Weather Icon"/>
     )
 };
 
