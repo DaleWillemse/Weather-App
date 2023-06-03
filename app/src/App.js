0361b912f8a85679cdf62cbcda6b04dddb15e428
Divan102 <divanswanepoel75@gmail.com>
2023-06-03 10:10:39 +0200

    Added forecast
    No css

--- app/src/App.js
+++ app/src/App.js
@@ -1,13 +1,14 @@
 import { useState } from "react";
 import "./App.css";
 import Search from "./components/search";
-import SearchBtn from "./components/searchBtn";
 import CurrentWeather from "./current-weather/current-weather";
+import Forecast from "./forecast/forecast";
 import SearchCity from "./components/searchCity";
 import { WEATHER_API_URL, WEATHER_API_KEY } from "./api";
 
 function App() {
   const [currentWeather, setCurrentWeather] = useState(null);
+  const [forecast, setForecast] = useState(null);
 
   const handleOnSearchChange = (searchData) => {
     const [lat, lon] = searchData.value.split(" ");
@@ -22,8 +23,10 @@ function App() {
     Promise.all([currentWeatherFetch, forecastFetch])
       .then(async (response) => {
         const weatherResponse = await response[0].json();
+        const forcastResponse = await response[1].json();
 
         setCurrentWeather({ city: searchData.label, ...weatherResponse });
+        setForecast({ city: searchData.label, ...forcastResponse });
       })
       .catch(console.log);
   };
@@ -31,15 +34,16 @@ function App() {
   return (
     <body>
       <div className="App">
-        <header className="App-header"> Weather Forecast</header>
+        <h5><header>Weather Forecast</header></h5>
         <p className="App-Paragraph">Enter your Zip Code to get started</p>
         <Search />
-        <SearchBtn />
+        
         
       </div>
-      <div className="container">
+      <div className="App-Search">
       <SearchCity onSearchChange={handleOnSearchChange} />
       {currentWeather && <CurrentWeather data={currentWeather} />}
+      {forecast && <Forecast data={forecast} />}
     </div>
     </body>
   );
