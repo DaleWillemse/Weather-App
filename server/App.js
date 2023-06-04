b82dcc6997ef88ad16910b7a4f193574b92a7536
Fenrir-23 <germangeneral2301@gmail.com>
2023-06-04 11:54:35 +0200

    Added Weather API Connection

--- server/App.js
+++ server/App.js
@@ -3,17 +3,25 @@ require('dotenv').config();
 
 //Port can't be the same as client/app
 const PORT = process.env.PORT;
-const KEY = process.env.KEY;
 
 //Build express server
 const express = require('express');
 const app = express();
 
-// Data Array
-const data = [];
+app.use(express.urlencoded({extended: true}));
+app.use(express.json); //Recieves json file
 
+// Import routes
+require('./routes/api/weatherApi');
+
+app.get('/', function(req, res){
+    res.send("API is working");
+});
 
 //runs server
-app.listen(PORT, () => {
+app.listen(PORT, (error) => {
+    if(error){
+        console.log(error.message);
+    };
     console.log(`Server is running on port: ${PORT}`);
 })
